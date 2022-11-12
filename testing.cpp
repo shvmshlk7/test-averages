dc2b0e3ef6ecabaaeaaf775894792f6c168f0d30
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-11-12 10:05:34 -0800

    Create testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b, c;
+	    cin >> a >> b >> c;
+	    if((a+b)/2.0 < 35 || (b+c)/2.0 < 35 || (a+c)/2.0 < 35){
+	        cout << "FAIL" << endl;
+	    }else{
+	        cout << "PASS" << endl;
+	    }
+	}
+	return 0;
+}
